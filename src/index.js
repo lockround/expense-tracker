f222a74b4e0ff986c72d3060d448b13ac4bd756e
Shubham Tiwari <websbiz0000@gmail.com>
2022-09-02 10:41:36 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import "@fontsource/roboto";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
